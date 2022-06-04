bcd2388432a7e744c8b895caf606c831d777b4c6
Yasin <yasin.zeighami@gmail.com>
2022-06-04 14:18:55 +0330

    Showing images in RecyclerView

---
+++ app/src/main/java/com/yasin/imagesearchapp/api/UnsplashResponse.kt
@@ -0,0 +1,8 @@
+package com.yasin.imagesearchapp.api
+
+import com.yasin.imagesearchapp.data.UnsplashPhoto
+
+data class UnsplashResponse (
+
+    val results:List<UnsplashPhoto>
+        )
---
+++ app/src/main/java/com/yasin/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,31 @@
+package com.yasin.imagesearchapp.ui.gallery
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.yasin.imagesearchapp.data.UnsplashRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GalleryViewModel @Inject constructor(private val repository: UnsplashRepository) :
+    ViewModel() {
+
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    val photos = currentQuery.switchMap { queryString ->
+        repository.getSearchResult(queryString).cachedIn(viewModelScope)
+
+    }
+
+    fun searchPhotos(query: String) {
+
+        currentQuery.value = query
+    }
+
+    companion object {
+
+        private const val DEFAULT_QUERY = "cats"
+    }
+}
