dc4d85f7311b2b0152bcd9e8db15450e86aec979
Yasin <yasin.zeighami@gmail.com>
2022-06-04 15:06:37 +0330

    details

---
+++ app/src/main/java/com/yasin/imagesearchapp/ui/details/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.yasin.imagesearchapp.ui.details
+
+import androidx.fragment.app.Fragment
+import com.yasin.imagesearchapp.R
+
+class DetailsFragment:Fragment(R.layout.fragment_details) {
+}
